a320e3bcac66d94995fed03fab94f370b43facf2
Julien SAVRY MOLINA <105850474+JSavryMolina@users.noreply.github.com>
2025-08-04 22:39:42 +0200

    Test of a website
    #Version_1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        matcha: '#3E5C45',
+        argile: '#C9975D',
+        washi: '#F6F2E8',
+        brun: '#3A2E2A',
+        vermillon: '#C75B57',
+        encre: '#2E4057',
+        feuille: '#D9E4C4',
+      }
+    }
+  }
+}
